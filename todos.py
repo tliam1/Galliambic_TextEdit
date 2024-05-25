7c8fe75b7c617005173db637eac9db0ceb522e96
tliam1 <97718694+tliam1@users.noreply.github.com>
2024-05-24 17:43:35 -1000

    Update todos.py

--- todos.py
+++ todos.py
@@ -1,8 +1,8 @@
 # TODO (Functionality): create a better saving system that has support for importing into other text docs (dump file doesnt translate)
 # TODO (BUG): fix line number error where wrapping fucks up the line count
-# removed wrapping for now. Good solution, but look into something better
+    # removed wrapping for now. Good solution, but look into something better
 # TODO (Functionality): create auto indent, auto capitalization, and if possible spelling error detection
-# auto indent is done
+    # auto indent is done
 
 
 # TODO (Functionality): highlight for mass changes (fonts, comments, etc)
@@ -10,8 +10,10 @@
 # TODO (BUG): If there are no words currently on the text doc, you can't change the font
 
 # TODO (Functionality): word coloring based on function, variable, etc
+    # in progress
 
 # TODO (Functionality): force cursor to the end of the line when switching lines
+    # Done
 
 # TODO (BUG): cant have multiple function highlights one the same line
 
